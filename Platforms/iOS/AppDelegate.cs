a8a27294ea7b6b86963faa302edfe038843abd2e
“valeriansoftware” <valerian.malofeev@yandex.ru>
2025-03-27 12:40:08 +0300

    Delete useless platforms

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace SmartFall;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
